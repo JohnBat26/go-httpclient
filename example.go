5a05309d2d1b9d25b782569956447aa99b9e3451
Eugene Batogov <johnbat26@gmail.com>
2020-11-04 15:24:07 +0300

    First test

--- example.go
+++ example.go
@@ -26,8 +26,26 @@ func main() {
 	getUrls()
 }
 
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
 func getUrls() {
 	response, err := githubHttpClient.Get("https://api.github.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(bytes))
+}
+
+func createUser(user User) {
+	response, err := githubHttpClient.Post("https://api.github.com", nil, user)
 	if err != nil {
 		panic(err)
 	}
--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -1,19 +1,43 @@
 package gohttp
 
 import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
 	"errors"
 	"net/http"
+	"strings"
 )
+
+func (c *httpClient) getRequestBody(contentType string, body interface{}) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	switch strings.ToLower(contentType) {
+	case "application/json":
+		return json.Marshal(body)
+	case "application/xml":
+		return xml.Marshal(body)
+	default:
+		return json.Marshal(body)
+	}
+}
 
 func (c *httpClient) do(method string, url string, headers http.Header, body interface{}) (*http.Response, error) {
 	client := http.Client{}
+	fullHeaders := c.getRequestHeaders(headers)
 
-	request, err := http.NewRequest(method, url, nil)
+	requestBody, err := c.getRequestBody(fullHeaders.Get("Content-Type"), body)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(requestBody))
 	if err != nil {
 		return nil, errors.New("unable to create a new request")
 	}
 
-	fullHeaders := c.getRequestHeaders(headers)
 	request.Header = fullHeaders
 
 	return client.Do(request)
