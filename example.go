21070558c2fe2a326b9529834eba09a19f2470f2
Eugene Batogov <johnbat26@gmail.com>
2020-11-04 12:45:50 +0300

    Create the core of http client

---
+++ example.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/JohnBat26/go-httpclient/gohttp"
+)
+
+func main() {
+	client := gohttp.New()
+
+	response, err := client.Get("https://api.github.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(bytes))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JohnBat26/go-httpclient
+
+go 1.15
